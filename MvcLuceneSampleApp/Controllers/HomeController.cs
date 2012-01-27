65327e57efd58bbf09cefb13f981babffc361c89
Mikhail T <siam.nanaah@gmail.com>
2012-01-27 13:46:15 -0600

    update2

--- MvcLuceneSampleApp/Controllers/HomeController.cs
+++ MvcLuceneSampleApp/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Web;
+﻿using System.IO;
 using System.Web.Mvc;
 using MvcLuceneSampleApp.Search;
 using MvcLuceneSampleApp.ViewModels;
--- MvcLuceneSampleApp/ViewModels/IndexViewModel.cs
+++ MvcLuceneSampleApp/ViewModels/IndexViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using MvcLuceneSampleApp.Search;
 
 namespace MvcLuceneSampleApp.ViewModels {
