ada301eb3c95564579cefebb252933810f280c57
Mikhail T <siam.nanaah@gmail.com>
2012-01-30 15:22:21 -0600

    update5

---
+++ MvcLuceneSampleApp/Model/SampleData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MvcLuceneSampleApp.Search {
+	public static class SampleDataRepository {
+		public static SampleData Get(int id) {
+			return GetAll().SingleOrDefault(x => x.Id.Equals(id));
+		}
+		public static List<SampleData> GetAll() {
+			return new List<SampleData> {
+			                            	new SampleData {Id = 1, Name = "Belgrad", Description = "City in Serbia"},
+			                            	new SampleData {Id = 2, Name = "Moscow", Description = "City in Russia"},
+			                            	new SampleData {Id = 3, Name = "Chicago", Description = "City in USA"},
+			                            	new SampleData {Id = 4, Name = "Mumbai", Description = "City in India"},
+			                            	new SampleData {Id = 5, Name = "Hong-Kong", Description = "City in Hong-Kong"},
+			                            };
+		}
+	}
+}
+namespace MvcLuceneSampleApp.Search {
+	public class SampleData {
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+	}
+}
